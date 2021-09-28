305d33c48dc4822a3dc2b0ddc378de7bfdc4d441
sato-miyu334 <ksw2070334@stu.o-hara.ac.jp>
2021-09-28 10:07:03 +0900

    確認その２

--- config/settings.py
+++ config/settings.py
@@ -166,7 +166,7 @@ ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_EMAIL_REQUIRED = True
 
 #ログイン/ログアウト後の遷移先を設定
-LOGIN_REDIRECT_URL = 'miyu:diary_list'
+LOGIN_REDIRECT_URL = 'miyu:index'
 ACCOUNT_LOGOUT_REDIRECT_URL = 'account_login'
 
 #ログアウトリンクのクリック一発でログアウトする設定
