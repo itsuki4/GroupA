24a66e99b955bf3649b9d27546bbbf2cb9bd0d4a
itsuki <ksw2070306@stu.o-hara.ac.jp>
2021-09-28 20:38:44 +0900

    運用

--- itsuki/urls.py
+++ itsuki/urls.py
@@ -5,9 +5,9 @@ app_name= 'itsuki'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('inquiry/', views.InquiryView.as_view(), name="inquiry"),
-    path('list/', views.DiaryListView.as_view(), name="diary_list"),
-    path('detail/<int:pk>/',views.DiaryDetailView.as_view(), name="diary_detail"),
-    path('create/', views.DiaryCreateView.as_view(), name="diary_create"),
-    path('update/<int:pk>/',views.DiaryUpdateView.as_view(),name="diary_update"),
-     path('delete/<int:pk>/',views.DiaryDeleteView.as_view(),name="diary_delete"),
+    path('list/', views.DiaryListView.as_view(), name="list"),
+    path('detail/<int:pk>/',views.DiaryDetailView.as_view(), name="detail"),
+    path('create/', views.DiaryCreateView.as_view(), name="create"),
+    path('update/<int:pk>/',views.DiaryUpdateView.as_view(),name="update"),
+    path('delete/<int:pk>/',views.DiaryDeleteView.as_view(),name="delete"),
 ]
